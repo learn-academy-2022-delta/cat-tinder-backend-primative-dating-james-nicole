a9a298548de5d06b6c4c613bdef7fbb78345e482
JICaddick <james_caddick16@hotmail.com>
2022-08-04 18:31:36 -0500

    moabugs fixed, 1,000 thank-you's Trish & Elyse

--- db/migrate/20220804220346_rename_column_to_image.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnToImage < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :monkeys, :imgage, :image
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_04_220346) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
 ActiveRecord::Schema[7.0].define(version: 2022_08_04_203117) do
   create_table "monkeys", force: :cascade do |t|
     t.string "name"
@@ -24,3 +20,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_04_203117) do
     t.datetime "updated_at", null: false
   end
 end
+
