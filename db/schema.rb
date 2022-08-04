a503050ab83206f1566360b0c1b341f3eec22cb9
Nicole <nicole.rivera061@gmail.com>
2022-08-04 13:53:10 -0700

    Added column through migrations to include image as text

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_04_183545) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_203117) do
   create_table "monkeys", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.text "enjoys"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,31 @@
 monkeys = [
     {
+        id: 1,
         name: "Felipe",
-        age: 7,
-        enjoys: "Sunshine and warm spots"
-    },
-    {
-        name: "Bertha",
-        age: 10,
-        enjoys: "Banana Splits"
-    },
-    {
-        name: "Stella",
         age: 3,
-        enjoys: "Getting all of the attention"
+        enjoys: "sunshine and warm spots",
+        image: "https://cdn.britannica.com/06/150806-050-6AE99C98/Proboscis-monkey.jpg?w=690&h=388&c=crop"
     },
     {
+        id: 2,
+        name: "Bertha",
+        age: 4,
+        enjoys: "banana splits",
+        image: "https://live.staticflickr.com/3946/15368916487_7063f08a82_b.jpg"
+    },
+    {
+        id: 3,
+        name: "Stella",
+        age: 1,
+        enjoys: "getting all the attention",
+        image: "https://st2.depositphotos.com/2927537/7025/i/950/depositphotos_70253417-stock-photo-funny-monkey-with-a-red.jpg"
+    },
+    {
+        id: 4,
         name: "Winston",
-        age: 11,
-        enjoys: "Eating all the ticks"
+        age: 5,
+        enjoys: "eating all the ticks",
+        image: "https://media.npr.org/blogs/monkeysee/2009/10/22/angrymonkey-480fe8cb9c9c711f76b537f3a7f7e186696e1b0c-s1100-c50.jpg"
     }
 ]
 
