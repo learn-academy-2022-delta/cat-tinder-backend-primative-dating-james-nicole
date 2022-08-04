8af51dd4fa7c9197e7495f794d0f2948931cbb9d
JICaddick <james_caddick16@hotmail.com>
2022-08-04 18:16:25 -0500

    this one was a lot easier

--- spec/requests/monkeys_spec.rb
+++ spec/requests/monkeys_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Monkeys", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+	describe "GET /index" do
+		it "gets a list of monkeys" do
+			Monkey.create(
+        name: "Felipe",
+        age: 3,
+        enjoys: "sunshine and warm spots",
+        image: "https://cdn.britannica.com/06/150806-050-6AE99C98/Proboscis-monkey.jpg?w=690&h=388&c=crop"
+			)
+			get '/monkeys'
+			monkey = JSON.parse(response.body)
+			expect(response).to have_http_status(200)
+			expect(monkey.length).to eq 1
+		end
+	end
 end
